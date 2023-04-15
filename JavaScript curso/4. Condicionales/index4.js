9e308e331a78cccabf80e02a00c2baee7015755b
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2023-04-16 01:29:35 +0200

    Add files via upload

---
+++ JavaScript curso/4. Condicionales/index4.js
@@ -0,0 +1,20 @@
+//Condicional if
+var numero1 = 8;
+if(numero1==7){
+    console.log("El número es 7");
+}
+else if(numero1==8){
+    console.log("El número es el 8");
+}
+else{
+    console.log("El número no es 7");
+}
+
+//número par-impar. Todo número dividido por 2 de un resto de 0, es par.
+
+if(numero1 %2==0){
+    console.log("El número es par");
+}
+else{
+    console.log("El número es impar");
+}
